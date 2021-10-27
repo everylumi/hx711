3206a8e4966fdb3711ce7095724ae29203b33f47
Github <Github>
2021-10-27 23:00:44 +0900

    [Issued at Oct-27, 2021]

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
 
 setup(
     name='hx711',
-    packages=['hx711'],
+    packages=['.'],
     version='1.0.0',
     description='hx711 chip interface library',
-    author_email='everylumi@gmail.com',
-    classifiers=[
-    'Programming Language :: Python :: 3',
-    'License :: BSD 3-Clause',
-    'Operating System :: OS Independent',
-    ],
-    keywords='hx711 raspberry',
-    url='https://github.com/everylumi/hx711',
+    author_email="everylumi@gmail.com",
+    url="https://github.com/everylumi/hx711",
     python_requires='>=3',
     zip_safe=False,
+    long_description=readme()
 )
